792fa7cd316ab1aa6acf930f3bf41ebc14a5298b
sgamage2018 <sgamage2018@my.fit.edu>
2019-03-03 15:41:53 -0500

    Initial commit - ESLint config and package installation

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+
+const App = () => (
+  <div className="App">
+    <p>Hello from todo-app</p>
+  </div>
+);
+
+export default App;
